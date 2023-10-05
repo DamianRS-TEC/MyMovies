10c94020c5bdb7d8aa19002b0f565c0504021415
Ubuntu <ubuntu@ip-172-31-56-148.ec2.internal>
2023-10-05 21:45:02 +0000

    Django Setup

--- movies/movies/settings.py
+++ movies/movies/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-i(n(0z0xy1j%q$8_^b(+05)_bm8iqwr%nyh)jzls3t*xqqch7_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['d127web.ddns.net']
 
 
 # Application definition
