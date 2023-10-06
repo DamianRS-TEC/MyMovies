c9ce0321366c0a5e444a76c96d4814ce2e3e4cba
Ubuntu <ubuntu@ip-172-31-56-148.ec2.internal>
2023-10-06 21:03:10 +0000

    model

--- movies/mymovies/models.py
+++ movies/mymovies/models.py
@@ -1,3 +1,30 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Job(models.Model):
+    name = modedls.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Person(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    overview = models.TextField()
+    release_date = models.DateTimeField()
+    running_time = models.IntegerField()
+    budget = models.IntegerField(blank=True)
+    tmdb_id = models.IntegerField(blank=True, unique=True)
