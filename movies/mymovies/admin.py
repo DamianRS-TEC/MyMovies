97be10b19bff3b263e56ee049dbb9df7473d5ed5
Ubuntu <ubuntu@ip-172-31-56-148.ec2.internal>
2023-10-06 21:43:29 +0000

    Another

--- movies/mymovies/admin.py
+++ movies/mymovies/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 
 from mymovies.models import Movie, Genre, Job, Person, MovieCredit, MovieReview 
 
+class MovieCreditInline(admin.StackedInline):
+    model = MovieCredit
+
+class MovieAdmin(admin.ModelAdmin):
+    filter_horizontal = ('credits', )
+    inlines = (MovieCreditInline)
+
+class MovieCreditAdmin(admin.ModelAdmin):
+    search_fields = ['']
+
 admin.site.register(Movie)
 admin.site.register(Genre)
 admin.site.register(Job)
